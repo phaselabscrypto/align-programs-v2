125cda4bf9c7cd57bc8c8a6393187d0ab5bd0d73
Solanamonk <solanamonk@protonmail.com>
2024-06-29 02:33:14 +0200

    refactor initialize proposal by wallet

--- programs/org-guard/src/instructions/initialize_proposal_by_wallet_v0.rs
+++ programs/org-guard/src/instructions/initialize_proposal_by_wallet_v0.rs
@@ -16,17 +16,23 @@ pub fn handler<'info>(
 ) -> Result<()> {
     let base = &ctx.accounts.initialize_proposal_base;
 
-    let config = match &base.guard.guard_type {
+    assert_sufficient_weight(&base.guard.guard_type, &ctx.accounts.proposer)?;
+
+    cpi_initialize_proposal(&base, args)
+}
+
+fn assert_sufficient_weight(guard_type: &GuardType, proposer: &AccountInfo) -> Result<()> {
+    let config = match guard_type {
         GuardType::WalletList { guard_data } => guard_data
             .iter()
-            .find(|config| config.address == ctx.accounts.proposer.key())
+            .find(|config| config.address == proposer.key())
             .ok_or(ErrorCode::ProposerNotValid)
             .cloned(),
         _ => Err(ErrorCode::InstructionNotAllowed.into()),
     }?;
 
     if config.multiplier > 0 {
-        cpi_initialize_proposal(&base, args)
+        Ok(())
     } else {
         Err(ErrorCode::InsufficientWeight.into())
     }
