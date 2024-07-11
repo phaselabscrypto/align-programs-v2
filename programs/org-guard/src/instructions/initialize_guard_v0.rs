088a10b8fdb1f88bfe6cda068c13d206b233dbdb
Solanamonk <solanamonk@protonmail.com>
2024-07-11 10:59:05 +0200

    remove redundant 80 bytes from guard account

--- programs/org-guard/src/instructions/initialize_guard_v0.rs
+++ programs/org-guard/src/instructions/initialize_guard_v0.rs
@@ -15,7 +15,7 @@ pub struct InitializeGuardV0<'info> {
     #[account(
         init,
         payer = payer,
-        space = 8 + 80 + GuardV0::INIT_SPACE,
+        space = 8 + GuardV0::INIT_SPACE,
         seeds = [b"guard", args.name.as_bytes()],
         bump
     )]
